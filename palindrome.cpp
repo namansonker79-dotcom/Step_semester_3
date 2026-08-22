e35d4c2ab4e792c683d48e22f5ab4e4fb72cf2b4
Naman Sonker <namansonker79@gmail.com>
2026-08-22 15:47:18 +0530

    Added palindrome program

---
+++ palindrome.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, original, reversed = 0, digit;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    original = n;
+
+    while (n != 0) {
+        digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n = n / 10;
+    }
+
+    if (original == reversed) {
+        cout << "The number is a Palindrome.";
+    } else {
+        cout << "The number is not a Palindrome.";
+    }
+
+    return 0;
+}
